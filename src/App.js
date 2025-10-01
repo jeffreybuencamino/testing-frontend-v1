3fb5c0a18ae98fcbe2e3edd092fa842e7cb38ade
Your Name <your_email@example.com>
2025-09-30 18:06:05 -0700

    Experimenting with routes update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"; 
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  
+
+  const handleAddBook = () => {
+
+    const bookData = {
+      
+      title: "Title of book",
+      author: "Jeffrey Buencamino",
+      pages: 328,
+      genres: [
+        "Sci-fi",
+        "Mystery"
+      ],
+      rating: 9
+    }
+
+    fetch('http://localhost:3000/test-api', {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(bookData)
+    })
+    .then((res) => res.json())
+    .then((data) => {console.log("Response from server: ", data.title)})
+    .catch((err) => { console.log("Failed to fetch: ", err)})
+
+  }
+
+
+
+    // GET request example
+  useEffect(() => {
+    fetch("http://localhost:3000/api/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>message from backend: { message}</h1>
+      <button onClick={handleAddBook}>Click me</button>
     </div>
   );
 }
